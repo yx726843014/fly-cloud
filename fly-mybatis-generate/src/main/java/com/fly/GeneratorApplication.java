733d2f970eabaf727067128ef9f78edbd8fbd6b3
youxiong <726843014@qq.com>
2018-10-03 18:07:08 +0800

    mybatis自动生成实体类

--- fly-mybatis-generate/src/main/java/com/fly/GeneratorApplication.java
+++ fly-mybatis-generate/src/main/java/com/fly/GeneratorApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class GeneratorApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(GeneratorApplication.class, args);
     }
